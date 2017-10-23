1811f73814fc663b6f3374a996a83baa7e472e52
xiaoliangliting <1140087655@qq.com>
2017-10-23 16:09:46 +0800

    登录注册修改

--- js/login.js
+++ js/login.js
@@ -18,8 +18,7 @@ $(function(){
             $(".txt1").html("");
         }
     });
-    //点击事件
-    //单个账号登录
+
     $("#btnOnclick").click(function(){
         var phone=$("#phone").val();//手机号码
         var password=$("#password").val();//密码
@@ -31,17 +30,42 @@ $(function(){
                 $(".txt1").html("*密码不为空！");
             }
         }
-        else if(phone=="admin" && password=="123456"){
-
-            //location.href="222.html?txt="+$("#txt").val();//发送txt里面的内容
-            window.location.href="./userCenter.html?phone="+$("#phone").val();
-
+        else if(phone==sessionStorage.getItem("userName") && password==sessionStorage.getItem("userPaw")){
+            sessionStorage.setItem("phone",$("#phone").val());//setItem（key,value）;
+            window.location.href="./userCenter.html";
         }
         else{
             $(".txt1").html("账号或密码不对！");
         }
     });
+    //点击事件
+    //单个账号登录
+    //方法2
+    /***
+     *
+    $("#btnOnclick").click(function(){
+        var phone=$("#phone").val();//手机号码
+        var password=$("#password").val();//密码
+        if(isBlank(phone) || isBlank(password)){
+            if(isBlank(phone)){
+                $(".txt").html("*手机号码不为空！");
+            }
+            if(isBlank(password)){
+                $(".txt1").html("*密码不为空！");
+            }
+        }
+        else if(phone=="梁丽婷" && password=="123456"){
 
+            //location.href="222.html?txt="+$("#txt").val();//发送txt里面的内容
+           // window.location.href="./userCenter.html?phone="+$("#phone").val();//取值第一种方法
+            sessionStorage.setItem("phone",$("#phone").val());//setItem（key,value）;
+            window.location.href="./userCenter.html";
+        }
+        else{
+            $(".txt1").html("账号或密码不对！");
+        }
+    });
+     **/
     //多个账号登录时
     //var UserName=[
     //    ["admin","123456"], ["13531409665","123456"]
